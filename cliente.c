6f202d1a89d941b0f9dbda977314c5a321473c73
anviol <andrev-eng@ufmg.br>
2016-10-24 18:50:36 -0200

    Coloquei UDP com IPV6 e IPV4

--- cliente.c
+++ cliente.c
@@ -7,19 +7,75 @@
 #define LEN 4096
 
 int main(int argc, char **argv){
+	//struct addrinfo Hints, *Res=NULL;
+    //struct sockaddr_in6 ServerAddr;
+    /*memset(&Hints, 0x00, sizeof(Hints));
+    Hints.ai_flags = AI_V4MAPPED;
+    Hints.ai_family = AF_UNSPEC;
+    Hints.ai_socktype = SOCK_DGRAM;
+    Rc = inet_pton(AF_INET, argv[1], &ServerAddr);
+    if (Rc == 1){ // endereco valido de IPv4?
+       Hints.ai_family = AF_INET;
+       Hints.ai_flags = AI_NUMERICHOST;
+    }
+    else{
+       Rc = inet_pton(AF_INET6, argv[1], &ServerAddr);
+       if (Rc == 1){ // endereco valido de IPv6?
+          Hints.ai_family = AF_INET6;
+          Hints.ai_flags = AI_NUMERICHOST;
+       }
+    }
+    getaddrinfo(argv[1], argv[2], &Hints, &Res); 
+    UdpSocket = socket(Res->ai_family, Res->ai_socktype, Res->ai_protocol);*/
 
-	struct sockaddr_in servidor,cliente; 
-	int tam_cliente, meuSocket, conectado = 1, numeroMensagem = 0, slen = 0, i = 0;
+    struct addrinfo auxiliar, *socketInfo;
+	struct sockaddr_in6 servidor; 
+	int tamanhoServidor, meuSocket, conectado = 1, numeroMensagem = 0, slen = 0, i = 0;
 	char envia[556];
 	char recebe[556];
 	char buffer[LEN];
 
-	meuSocket = socket(PF_INET, SOCK_DGRAM, 0); 
-	servidor.sin_family = AF_INET; // familia de endereços
-	servidor.sin_port = htons(atoi(argv[2])); // porta
+	tamanhoServidor = sizeof(struct sockaddr_in6);
+
+	meuSocket = socket(PF_INET6, SOCK_DGRAM,0);
+
+	memset(&servidor, 0x0, sizeof servidor);
+
+	servidor.sin6_port = htons(atoi(argv[2]));
+	servidor.sin6_family = AF_INET6;
+	servidor.sin6_addr = in6addr_any;
+
+	bind(meuSocket, (struct sockaddr *)&servidor, tamanhoServidor);
+
+	memset(&auxiliar, 0x0, sizeof auxiliar);
+	memset(&socketInfo, 0x0, sizeof socketInfo);
+
+	auxiliar.ai_flags = 0;
+	auxiliar.ai_family = PF_INET6;
+	auxiliar.ai_socktype = SOCK_DGRAM;
+	auxiliar.ai_protocol = IPPROTO_UDP;
+	getaddrinfo(argv[1], argv[2], &auxiliar, &socketInfo);
+
+    /*if (inet_pton(AF_INET, argv[1], &servidor) == 1){ // endereco valido de IPv4?
+       auxiliar.ai_family = AF_INET;
+       auxiliar.ai_flags = AI_NUMERICHOST;
+    }
+    else if (inet_pton(AF_INET6, argv[1], &servidor) == 1){ // endereco valido de IPv6?
+          auxiliar.ai_family = AF_INET6;
+          auxiliar.ai_flags = AI_NUMERICHOST;
+       }
+    else {
+    	printf("Endereço não localizado!\n");
+    }
+
+    getaddrinfo(argv[1], argv[2], &auxiliar, &socketInfo);
+
+	meuSocket = socket(socketInfo->ai_family, socketInfo->ai_socktype, socketInfo->ai_protocol); 
+	//servidor.sin_family = AF_INET; // familia de endereços
+	//servidor.sin_port = htons(atoi(argv[2])); // porta
 	// para usar um ip qualquer use inet_addr("10.10.10.10"); ao invés de htonl(INADDR_ANY)
 	//servidor.sin_addr.s_addr=htonl(INADDR_ANY);
-	servidor.sin_addr.s_addr = inet_addr(argv[1]);
+	//servidor.sin_addr.s_addr = inet_addr(argv[1]);*/
 
 	memset(&envia, 0x0, sizeof envia);
 	memset(&recebe, 0x0, sizeof recebe);
@@ -40,23 +96,21 @@ int main(int argc, char **argv){
 		switch (buffer[0]){
 			//Enviando para o servidor comando de gravação de tempo com o tempo a ser gravado
 			case 'D': case 'd':
-				sendto(meuSocket, buffer, strlen(buffer), 0, (struct sockaddr*)&servidor, sizeof(struct sockaddr_in));
+				sendto(meuSocket, buffer, strlen(buffer), 0, (struct sockaddr*)&servidor, sizeof(struct sockaddr_in6));
 				numeroMensagem++;//Contando quantas mensagens foram enviadas
-				tam_cliente = sizeof(struct sockaddr_in);
-				recvfrom(meuSocket, recebe, 556, MSG_WAITALL, (struct sockaddr*)&cliente, &tam_cliente);
+				recvfrom(meuSocket, recebe, 556, MSG_WAITALL, (struct sockaddr*)&servidor, &tamanhoServidor);
 				printf("Recebi:%s\n",recebe);
 			break;
 
 			//Enviando para o servidor comando para retornar o valor de uma certa posição
 			//e recebendo o que se pede ao servidor
 			case 'C': case 'c':
-				sendto(meuSocket, buffer, strlen(buffer), 0, (struct sockaddr*)&servidor, sizeof(struct sockaddr_in));//Enviando solicitação para o servidor
-				tam_cliente = sizeof(struct sockaddr_in);
-				recvfrom(meuSocket, recebe, 556, MSG_WAITALL, (struct sockaddr*)&cliente, &tam_cliente);
+				sendto(meuSocket, buffer, strlen(buffer), 0, (struct sockaddr*)&servidor, sizeof(struct sockaddr_in6));//Enviando solicitação para o servidor
+				recvfrom(meuSocket, recebe, 556, MSG_WAITALL, (struct sockaddr*)&servidor, &tamanhoServidor);
 				printf("Recebi:%s\n",recebe);
-				sendto(meuSocket, "ACK!", strlen("ACK!"), 0
-					, (struct sockaddr*)&cliente, sizeof(struct sockaddr_in));
-				slen = recvfrom(meuSocket, buffer, 556, MSG_WAITALL, (struct sockaddr*)&cliente, &tam_cliente);//Recebendo do servidor o tempo solicitado
+				/*sendto(meuSocket, "ACK!", strlen("ACK!"), 0
+					, (struct sockaddr*)&servidor, sizeof(struct sockaddr_in6));*/
+				slen = recvfrom(meuSocket, buffer, 556, MSG_WAITALL, (struct sockaddr*)&servidor, &tamanhoServidor);//Recebendo do servidor o tempo solicitado
 				buffer[slen - 1] = '\0';//Tirando o excesso de \n
 				printf(">Servidor: %s\n", buffer);//Mostrando o tempo para o usuário
 			break;
@@ -64,25 +118,25 @@ int main(int argc, char **argv){
 			//Enviando para o servidor comando para retornar os valores de todas posições me ordem
 			//e recebendo o que se pede ao servidor
 			case 'O': case 'o':
-				sendto(meuSocket, buffer, strlen(buffer), 0, (struct sockaddr*)&servidor, sizeof(struct sockaddr_in));//Enviando comando solicitado
-				tam_cliente = sizeof(struct sockaddr_in);
-				recvfrom(meuSocket, recebe, 556, MSG_WAITALL, (struct sockaddr*)&cliente, &tam_cliente);
+				sendto(meuSocket, buffer, strlen(buffer), 0, (struct sockaddr*)&servidor, sizeof(struct sockaddr_in6));//Enviando comando solicitado
+				tamanhoServidor = sizeof(struct sockaddr_in6);
+				recvfrom(meuSocket, recebe, 556, MSG_WAITALL, (struct sockaddr*)&servidor, &tamanhoServidor);
 				printf("Recebi:%s\n",recebe);
 				for (i = 0; i < numeroMensagem; ++i){//Recebendo todas as mensagens de acordo com a quantidade enviada
-					slen = recvfrom(meuSocket, buffer, 556, MSG_WAITALL, (struct sockaddr*)&cliente, &tam_cliente);//Recebendo a posição de acordo com o contador
+					slen = recvfrom(meuSocket, buffer, 556, MSG_WAITALL, (struct sockaddr*)&servidor, &tamanhoServidor);//Recebendo a posição de acordo com o contador
 					buffer[slen - 1] = '\0';//Tirando o excesso de \n
 					printf(">Servidor: %s\n", buffer);//Mostrando para o usuário a resposta do servidor
 					sendto(meuSocket, "ACK!", strlen("ACK!"), 0
-						, (struct sockaddr*)&cliente, sizeof(struct sockaddr_in));//Enviando para o servidor confirmação de recebimento
+						, (struct sockaddr*)&servidor, sizeof(struct sockaddr_in6));//Enviando para o servidor confirmação de recebimento
 				}				
 			break;
 
 			//Enviando para o servidor o comando para fechar
 			//e fechando a si mesmo
 			case 'Z': case 'z':
-				sendto(meuSocket, buffer, strlen(buffer), 0, (struct sockaddr*)&servidor, sizeof(struct sockaddr_in));//Enviando comando solicitado
-				tam_cliente = sizeof(struct sockaddr_in);
-				recvfrom(meuSocket, recebe, 556, MSG_WAITALL, (struct sockaddr*)&cliente, &tam_cliente);//Enviando para o servidor o comando de fechar
+				sendto(meuSocket, buffer, strlen(buffer), 0, (struct sockaddr*)&servidor, sizeof(struct sockaddr_in6));//Enviando comando solicitado
+				tamanhoServidor = sizeof(struct sockaddr_in6);
+				recvfrom(meuSocket, recebe, 556, MSG_WAITALL, (struct sockaddr*)&servidor, &tamanhoServidor);//Enviando para o servidor o comando de fechar
 				conectado = 0;//Saindo do loop
 			break;
 
--- servidor.c
+++ servidor.c
@@ -6,35 +6,54 @@
 
 #define LEN 4096
 
-int main(int argc, char **argv){
-	struct sockaddr_in cliente, servidor;
+int main(int argc, char **argv){	
+	/*struct sockaddr_in6 serv, client;
+    int s;
+ 	uint16_t Port;
+ 	int ClientAddrLen = sizeof(client);
+ 	Port = atoi(argv[1]);
+ 	s = socket(AF_INET6, SOCK_DGRAM, 0);	
+    memset(&serv, 0, sizeof(serv));
+    serv.sin6_family = AF_INET6;
+    serv.sin6_port   = htons(Port);
+    serv.sin6_addr   = in6addr_any;
+    bind(s,(struct sockaddr *)&serv,sizeof(serv));
+	listen(s, 10);*/
+
+
+
+
+
+
+	struct sockaddr_in6 cliente, servidor;
 	int tam_cliente, meuSocket, enviei = 0, contMensagens = 0, i = 0;
 	// MTU padrão pela IETF
 	char buffer[556];
 
 	Mensagem mesagensRecebidas[LEN/4];
 
-	meuSocket = socket(PF_INET ,SOCK_DGRAM ,0);
-	servidor.sin_family = AF_INET;
-	servidor.sin_port = htons(atoi(argv[1]));
-	servidor.sin_addr.s_addr = htonl(INADDR_ANY);
+	meuSocket = socket(AF_INET6 ,SOCK_DGRAM ,0);
+	memset(&servidor, 0, sizeof(servidor));
+	servidor.sin6_family = AF_INET6;
+	servidor.sin6_port = htons(atoi(argv[1]));
+	servidor.sin6_addr = in6addr_any;
 
 	memset(&buffer, 0x0, sizeof buffer);
 
-	bind(meuSocket, (struct sockaddr*)&servidor, sizeof(struct sockaddr_in));
+	bind(meuSocket, (struct sockaddr*)&servidor, sizeof(servidor));
 
 
 	printf("*** Servidor Ativo***\n");
 
 	while(1){		
-		tam_cliente = sizeof(struct sockaddr_in);
+		tam_cliente = sizeof(struct sockaddr_in6);
 		recvfrom(meuSocket, buffer, 556, MSG_WAITALL, (struct sockaddr*)&cliente, &tam_cliente);
 		arrumaMensagem(buffer);
 		printf(">Cliente: %s\n", buffer);
 		/*printf("Recebi:%s de <endereço:%s> <porta:%d>\n"
 			,buffer,inet_ntoa(cliente.sin_addr,ntohs(cliente.sin_port));*/
 		enviei = sendto(meuSocket, "ACK!", strlen("ACK!"), 0
-			, (struct sockaddr*)&cliente, sizeof(struct sockaddr_in));
+			, (struct sockaddr*)&cliente, sizeof(struct sockaddr_in6));
 		if (enviei >= 0)
 			printf("Envio de ACK!\n");
 		else{
@@ -59,13 +78,13 @@ int main(int argc, char **argv){
 						i = atoi(&buffer[2]);//Descobre qual posição o cliente quer
 						if (i <= contMensagens){//Verifica se a posição existe
 							sendto(meuSocket, mesagensRecebidas[i-1].texto, strlen(mesagensRecebidas[i-1].texto), 0
-								, (struct sockaddr*)&cliente, sizeof(struct sockaddr_in));
+								, (struct sockaddr*)&cliente, sizeof(struct sockaddr_in6));
 						}
 						else{
 							//Envia mensagem de erro, caso posição não exista
 							sendto(meuSocket, "Essa posição não foi informada.\n"
 								, strlen("Essa posição não foi informada.\n"), 0, (struct sockaddr*)&cliente
-								, sizeof(struct sockaddr_in));
+								, sizeof(struct sockaddr_in6));
 						}
 					break;
 
@@ -74,7 +93,7 @@ int main(int argc, char **argv){
 						//Envia para o cliente todas as mensagens de acordo com as mensagens recebidas
 						for (i = 0; i < contMensagens; ++i){
 							sendto(meuSocket, mesagensRecebidas[i].texto, strlen(mesagensRecebidas[i].texto), 0
-								, (struct sockaddr*)&cliente, sizeof(struct sockaddr_in));
+								, (struct sockaddr*)&cliente, sizeof(struct sockaddr_in6));
 							recvfrom(meuSocket, buffer, LEN, MSG_WAITALL, (struct sockaddr*)&cliente, &tam_cliente);
 						}
 					break;
@@ -90,7 +109,7 @@ int main(int argc, char **argv){
 					default:
 						sendto(meuSocket, "Erro na sintax da mensagem! A mensagem foi recusada.\n"
 							, strlen("Erro na sintax da mensagem! A mensagem foi recusada.\n"), 0
-							, (struct sockaddr*)&cliente, sizeof(struct sockaddr_in));
+							, (struct sockaddr*)&cliente, sizeof(struct sockaddr_in6));
 						printf(">Servidor: Erro na sintax da mensagem! A mensagem foi recusada.\n");
 		}				
 		memset(&buffer, 0x0, sizeof buffer);
